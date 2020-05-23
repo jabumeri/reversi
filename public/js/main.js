67b6e8ab89586de5dddcb47e1849bf14af61a66f
Jana Abumeri <jabumeri@uci.edu>
2020-05-23 13:02:53 -0700

    Lobby: Invite Button

--- public/js/main.js
+++ public/js/main.js
@@ -54,7 +54,7 @@ socket.on( 'join_room_response', function( payload ){
 		nodeB.append( '<h4>' + payload.username + '</h4>' );
 
 		nodeC.addClass( 'col-3 text-left' );
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton( payload.socket_id );
 		nodeC.append(buttonC);
 
 		nodeA.hide();
@@ -65,7 +65,7 @@ socket.on( 'join_room_response', function( payload ){
 		nodeB.slideDown( 1000 );
 		nodeC.slideDown( 1000 );
 	} else {
-		var buttonC = makeInviteButton();
+		var buttonC = makeInviteButton( payload.socket_id );
 		$( '.socket_' + payload.socket_id + ' button' ).replaceWith( buttonC );
 		dom_elements.slideDown( 1000 );
 	}
@@ -101,6 +101,34 @@ socket.on( 'player_disconnected', function( payload ){
 	newNode.slideDown( 1000 );
 });
 
+function invite( who ){ 
+	var payload = {};
+	payload.requested_user = who; 
+
+	console.log( '*** Client Log Message: \'invite\' payload: ' + JSON.stringify( payload ) );
+	socket.emit( 'invite', payload );
+}
+
+socket.on( 'invite_response', function( payload ){
+	if( payload.result == 'fail' ){
+		alert( payload.message);
+		return;
+	}
+
+	var newNode = makeInvitedButton(); 
+	$( '.socket_' + payload.socket_id + ' button' ).replaceWith( newNode );
+});
+
+socket.on( 'invited', function( payload ){
+	if( payload.result == 'fail' ){
+		alert(payload.message);
+		return;
+	}
+
+	var newNode = makePlayButton(); 
+	$( '.socket_' + payload.socket_id + ' button' ).replaceWith( newNode );
+});
+
 socket.on( 'send_message_response', function( payload ){
 	if( payload.result == 'fail' ){
 		alert(payload.message);
@@ -120,8 +148,29 @@ function send_message(){
 	socket.emit( 'send_message', payload ); 
 }
 
-function makeInviteButton() {
+function makeInviteButton( socket_id ) {
 	var newHTML = '<button type=\'button\' class=\'btn btn-outline-primary\'>Invite</button>';
+	var newNode = $( newHTML );
+	newNode.click( function(){
+		invite( socket_id );
+	});
+	return( newNode );
+}
+
+function makeInvitedButton() {
+	var newHTML = '<button type=\'button\' class=\'btn btn-primary\'>Invited</button>';
+	var newNode = $( newHTML );
+	return( newNode );
+}
+
+function makePlayButton() {
+	var newHTML = '<button type=\'button\' class=\'btn btn-success\'>Play</button>';
+	var newNode = $( newHTML );
+	return( newNode );
+}
+
+function makeEngageButton() {
+	var newHTML = '<button type=\'button\' class=\'btn btn-danger\'>Engaged</button>';
 	var newNode = $( newHTML );
 	return( newNode );
 }
--- server.js
+++ server.js
@@ -200,6 +200,73 @@ io.sockets.on( 'connection', function( socket ){
 		io.sockets.in( room ).emit( 'send_message_response', successData );
 		log( 'Message sent to room ' + room + ' by ' + username );
 	});
+
+	socket.on( 'invite', function( payload ){
+		log( 'invite with ' + JSON.stringify( payload ) );
+		
+		// payload is undefined 
+		if( typeof payload === 'undefined' || !payload ){
+			var error_message = 'invite had no payload, command aborted';
+			log( error_message );
+			socket.emit( 'invite_response' , {
+				result: 'fail',
+				message: error_message
+			});
+			return;
+		}
+
+	
+		// username is undefined 
+		var username = players[socket.id].username;
+		if( typeof username === 'undefined' || !username ){
+			var error_message = 'invite had no payload, command aborted';
+			log( error_message );
+			socket.emit( 'invite_response' , {
+				result: 'fail',
+				message: error_message
+			});
+			return;
+		}	
+
+		var requested_user = payload.requested_user;
+		if( typeof requested_user === 'undefined' || !requested_user ){
+			var error_message = 'invite didn\'t specify a requested_user, command aborted';
+			log( error_message );
+			socket.emit( 'invite_response' , {
+				result: 'fail',
+				message: error_message
+			});
+			return;
+		}	
+
+		var room = players[socket.id].room;
+		var roomObject = io.sockets.adapter.rooms[room]; 
+
+		if( !roomObject.sockets.hasOwnProperty( requested_user ) ){
+			var error_message = 'invite requested a user that wasn\'t in the room, command aborted';
+			log( error_message );
+			socket.emit( 'invite_response' , {
+				result: 'fail',
+				message: error_message
+			});
+			return;
+		}
+
+		var successData = {
+			result: 'success',
+			socket_id: requested_user
+		}
+
+		socket.emit( 'invite_response', successData );
+
+		var successData = {
+			result: 'success',
+			socket_id: socket.id
+		}
+
+		socket.to( requested_user ).emit( 'invited', successData );
+		log( 'invite successful' );
+	});
 })
 
 
